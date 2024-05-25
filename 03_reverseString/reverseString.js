2757182a1b17f86fd6eb06f0f89cb7bc56fc7805
Krzysztof Bobrowicz <krzysztofbobrowicz@icloud.com>
2024-05-25 12:40:42 +0200

    Finish excercise nr 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // let reversedString = ''
+    // const stringArray = string.split(" ");
+    // const reversedStringArray = stringArray.reverse();
+    // for (let i = 0; i < reversedStringArray.length; i++) {
+    //     let reverseString = reversedStringArray[i].split("").reverse().join("");
+    //     reversedString += (reverseString);
+    //     reversedString += " ";
+    // }
+    // return reversedString.slice(0, -1);
 
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
