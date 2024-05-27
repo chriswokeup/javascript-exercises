46ff69e12b04d63c7a3565dcfef0af9469815679
Krzysztof Bobrowicz <krzysztofbobrowicz@icloud.com>
2024-05-27 19:26:25 +0200

    Finish excercise nr 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let initialNum = 0;
+    if (firstInt < 0 || secondInt < 0 ||
+         !Number.isInteger(firstInt) || !Number.isInteger(secondInt)) {
+        return "ERROR";
+    }
+    if (firstInt < secondInt) {
+        for (let i = firstInt; i <= secondInt; i++) {
+            initialNum += i; 
+        }
+    } if (firstInt > secondInt) {
+        for (let i = secondInt; i <= firstInt; i++) {
+            initialNum += i; 
+        }
+    } 
+    return initialNum;    
 };
 
 // Do not edit below this line
